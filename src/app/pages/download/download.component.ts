f6874c27cd986fede7413b09265548685019bb9f
“Young <hogusong1@gmail.com>
2018-12-27 17:16:55 -0500

    created 'download image mode

--- src/app/pages/download/download.component.ts
+++ src/app/pages/download/download.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage } from 'angularfire2/storage';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-download',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./download.component.css']
 })
 export class DownloadComponent implements OnInit {
+  photosCollection: AngularFirestoreCollection<any>;
+  photoDoc: AngularFirestoreDocument<any>;
+  photos: any[];
+  img_url: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private db: AngularFirestore,
+              private storage: AngularFireStorage) {
+    this.photosCollection = this.db.collection('photos', ref => ref);
   }
 
+  ngOnInit() {
+    this.photosCollection.snapshotChanges().pipe(map(changes => {
+      return changes.map(action => {
+        const data = action.payload.doc.data();
+        return data;
+      })
+    })).subscribe(data => { this.photos = data })
+  }
+
+  showImage(name) {
+    this.storage.ref(name).getDownloadURL().subscribe(url => {
+      this.img_url = url;
+    });
+  }
 }
